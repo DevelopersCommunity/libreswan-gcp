996b093f297e9f0520d22153c2ede9da45067a6e
Alexandre Teoi <ateoi@microsoft.com>
2021-08-30 10:00:29 -0300

    use string interpolation instead of replaces

--- main.tf
+++ main.tf
@@ -66,18 +66,120 @@ resource "random_password" "psk" {
 
 locals {
   psk = random_password.psk.result
-  ddclient = replace(
-    replace(
-      replace(
-        replace(file("ddclient.conf"), "<server>", var.dyndns.server),
-        "<user>", var.dyndns.user),
-      "<password>", var.dyndns.password),
-    "<hostname>", var.hostname
-  )
-  ikev2psk = replace(
-    replace(file("ikev2-psk.conf"), "<ipsec_id>", var.ipsec_identifier),
-      "<hostname>", var.hostname
-  )
+  
+  ddclientconf = <<-EOT
+  protocol=dyndns2
+  use=web
+  server=${var.dyndns.server}
+  ssl=yes
+  login=${var.dyndns.user}
+  password='${var.dyndns.password}'
+  ${var.hostname}
+  EOT
+
+  ddclient = <<-EOT
+  # Configuration for ddclient scripts
+  #
+  # /etc/default/ddclient
+
+  # Set to "true" if ddclient should be run every time DHCP client ('dhclient'
+  # from package isc-dhcp-client) updates the systems IP address.
+  run_dhclient="false"
+
+  # Set to "true" if ddclient should be run every time a new ppp connection is
+  # established. This might be useful, if you are using dial-on-demand.
+  run_ipup="false"
+
+  run_daemon="true"
+
+  # Set the time interval between the updates of the dynamic DNS name in seconds.
+  # This option only takes effect if the ddclient runs in daemon mode.
+  daemon_interval="5m"
+  EOT
+
+  ikev2psk = <<-EOT
+  conn ikev2-psk-${var.ipsec_identifier}
+    authby=secret
+    left=%defaultroute
+    leftid=@${var.hostname}
+    leftsubnet=0.0.0.0/0
+    # Clients
+    right=%any
+    # your addresspool to use
+    # you might need NAT rules if providing full internet to clients
+    rightaddresspool=192.168.66.1-192.168.66.254
+    rightid=@${var.ipsec_identifier}
+    #
+    # connection configuration
+    # DNS servers for clients to use
+    modecfgdns=8.8.8.8,8.8.4.4
+    narrowing=yes
+    # recommended dpd/liveness to cleanup vanished clients
+    dpddelay=30
+    dpdtimeout=120
+    dpdaction=clear
+    auto=add
+    ikev2=insist
+    rekey=no
+    # ikev2 fragmentation support requires libreswan 3.14 or newer
+    fragmentation=yes
+    # optional PAM username verification (eg to implement bandwidth quota
+    # pam-authorize=yes
+    ike=aes_gcm-aes_xcbc,aes_cbc-sha2
+  EOT
+
+  config = <<-EOT
+  #cloud-config
+
+  packages:
+    - libreswan
+    - nftables
+    - ddclient
+  package_update: true
+  package_upgrade: true
+  package_reboot_if_required: true
+
+  write_files:
+    - encoding: b64
+      content: ${base64encode(file("nftables.sh"))}
+      owner: root:root
+      path: /run/vpn/nftables.sh
+      permissions: '0700'
+    - encoding: b64
+      content: ${base64encode(local.ddclientconf)}
+      owner: root:root
+      path: /etc/ddclient.conf
+      permissions: '0600'
+    - encoding: b64
+      content: ${base64encode(local.ddclient)}
+      owner: root:root
+      path: /etc/default/ddclient
+      permissions: '0600'
+    - encoding: b64
+      content: ${base64encode(local.ikev2psk)}
+      owner: root:root
+      path: /etc/ipsec.d/ikev2-psk-${var.ipsec_identifier}.conf
+    - encoding: b64
+      content: ${
+        base64encode(
+          "@${var.ipsec_identifier} @${var.hostname}: PSK \"${local.psk}\""
+        )
+      }
+      owner: root:root
+      path: /etc/ipsec.d/ikev2-psk.secrets
+      permissions: '0600'
+
+  runcmd:
+    - [ /run/vpn/nftables.sh ]
+    - [ sed,
+      -i,
+      "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g",
+      /etc/sysctl.conf ]
+    - [ sysctl, -w, net.ipv4.ip_forward=1 ]
+    - [ systemctl, enable, ipsec.service ]
+    - [ systemctl, start, ipsec.service ]
+    - [ systemctl, restart, ddclient.service ]
+  EOT
 }
 
 resource "google_compute_firewall" "vpn" {
@@ -97,23 +199,7 @@ resource "google_compute_instance" "vpn" {
   can_ip_forward          = true
   tags                    = ["vpn-server"]
   metadata                = {
-    "user-data"       = replace(
-      replace(
-        replace(
-          replace(
-            replace(
-              file("config.yaml"),
-              "<nftables.sh>",
-              base64encode(file("nftables.sh"))),
-            "<ddclient.conf>", base64encode(local.ddclient)
-          ), "<ipsec_id>", var.ipsec_identifier
-        ), "<ikev2-psk.conf>", base64encode(local.ikev2psk)
-      ),
-      "<ikev2-psk.secrets>",
-      base64encode(
-        "@${var.ipsec_identifier} @${var.hostname}: PSK \"${local.psk}\""
-      )
-    )
+    "user-data" = local.config
   }
 
   boot_disk {
@@ -148,4 +234,5 @@ output "ipsec_identifier" {
 output "ipsec_pre_shared_key" {
   value       = local.psk
   description = "IPSec pre-shared key."
+  sensitive   = true
 }
